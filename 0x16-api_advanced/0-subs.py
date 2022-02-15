9c3c7fa27d4ea67a8608a6916985c58ecfbbc4a8
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-02-15 13:26:37 -0500

    [UPDATED] redirects are unavailable

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
 """
 Module for number_of_subscribers function
 """
@@ -14,7 +14,8 @@ def number_of_subscribers(subreddit):
     user_agent = {'User-agent': 'Mozilla/5.0'}
     resp = requests.get(
         'https://www.reddit.com/r/' + subreddit
-        + '/about.json', headers=user_agent
+        + '/about.json', headers=user_agent,
+        allow_redirects=False
         )
     data = resp.json()
     if data.get('error', 200) == 404:
